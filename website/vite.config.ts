24c046f4001d11f83a6d6545c49b8f3ebe461ac5
Eliza P <epetrycka@student.agh.edu.pl>
2024-11-10 23:27:58 +0100

    Added files, docker is not composing

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      ignored: ["**/node_modules/**", "**/dist/**"],
+    },
+  },
+
   plugins: [react()],
-})
+});
+
