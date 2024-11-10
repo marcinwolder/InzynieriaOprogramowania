bdfcd8c5f2f7c0c3b1d68d8d870ef0b86c11396d
marcinwolder <marcinwolder7@gmail.com>
2024-11-10 18:17:59 +0100

    Project restructure

---
+++ website/src/routes/main/OfferTable.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const OfferTable: React.FC;
+
+export default OfferTable;
---
+++ website/src/routes/main/index.tsx
@@ -0,0 +1,60 @@
+import clsx from "clsx";
+import Typewriter from "./Typewriter";
+import { FaArrowDown } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import OfferTable from "./OfferTable";
+import { FaArrowTurnDown } from "react-icons/fa6";
+import logoSrc from "/logo.jpg"
+
+export default function Main() {
+    const [scrolled, setScrolled] = useState(false)
+    useEffect(()=>{
+        const func = ()=>{
+            if (window.scrollY === 0) {
+                setScrolled(false)
+            } else {
+                setScrolled(true)
+            }
+        }
+        func()
+        window.addEventListener("scroll", func)
+        return ()=>{
+            window.removeEventListener("scroll", func)
+        }
+    }, [window.scrollY])
+
+    return (
+        <>
+            <div className="flex px-8 items-center gap-8 w-fit h-screen -mt-16 mx-auto">
+                <img className="h-96" src={logoSrc} alt="logo" />
+                <div className="">
+                    <h1 className="text-6xl font-serif">Twój hosting serwerów<br/>do grania w kratkę</h1>
+                    <Typewriter />
+                </div>
+            </div>
+            <div className="h-screen anchor-sklep flex items-center justify-center">
+                <div className="min-h-[32em]">
+                    <OfferTable/>
+                </div>
+            </div>
+            {/* Fixed Buttons */}
+            <div className={clsx("fixed w-full transition-all", {"-bottom-16": scrolled, "bottom-0": !scrolled})}>
+                <div onClick={()=>{
+                    if (!scrolled) {
+                        setScrolled(true);
+                        scrollTo({behavior: 'smooth', top: document.querySelector('.anchor-sklep')?.getBoundingClientRect().top});
+                    }
+                }} className="bg-info text-info-content w-fit flex items-start gap-4 mx-auto px-6 pt-4 rounded-t-xl cursor-pointer">
+                    <FaArrowDown className="text-2xl h-10 duration-400 animate-bounce-slow"/>
+                    <p className="font-semibold">Sprawdź ofertę</p>
+                </div>
+            </div>
+            <div className={clsx("fixed bottom-2 bg-info text-info-content rotate-180 rounded-full rounded-tl-none aspect-square w-10 flex items-center justify-center transition-all cursor-pointer", {"right-2": scrolled, "-right-12": !scrolled})}
+                onClick={()=>{
+                    scrollTo({top: 0, behavior: "smooth"})
+                }}>
+                <FaArrowTurnDown/>
+            </div>
+        </>
+    )
+}
