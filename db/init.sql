24b1f91c78c7e4b40cf03deef4763832b0901809
marcinwolder <marcinwolder7@gmail.com>
2024-11-24 12:39:56 +0100

    SQLAlchemy model done

--- db/init.sql
+++ db/init.sql
@@ -33,7 +33,7 @@ CREATE TABLE
         user_id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(30) NOT NULL,
         email VARCHAR(50) NOT NULL,
-        password VARCHAR(50) NOT NULL,
+        password VARCHAR(30) NOT NULL,
         registration_date DATE NOT NULL,
         recovery_hash VARCHAR(32)
     );
